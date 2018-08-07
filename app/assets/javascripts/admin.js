5709675de570c93d7584e3962beac2c8c0192e92
Shashi Singh <shashi.we@gmail.com>
2018-08-07 14:17:59 +0530

    added page to custom layout

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,16 @@
+//= require jquery
+//= require rails-ujs
+//= require popper
+//= require bootstrap
+
+//= require admin/perfect-scrollbar.jquery.min
+//= require admin/waves
+//= require admin/sidebarmenu
+//= require admin/custom.min
+//= require admin/jquery-jvectormap-2.0.2.min
+//= require admin/jquery-jvectormap-world-mill-en
+//= require admin/jquery.sparkline.min
+//= require admin/chartist.min
+//= require admin/chartist-plugin-tooltip.min
+//= require admin/dashboard4
+//= require admin/jQuery.style.switcher
