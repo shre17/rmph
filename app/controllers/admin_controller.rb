77c4919c5434d59fc23131a102d61300a3263675
Shashi Singh <shashi.we@gmail.com>
2018-08-08 17:03:54 +0530

    added dashboard pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,17 @@
 class AdminController < ApplicationController
-
+  before_action :authenticate_user!
   layout 'admin'
 
+  def referral
+    @user = current_user
+  end 
+
+  def direct_team
+  end
+
+  def wallet_transfer
+  end
+
+  def upgrade
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
     end 
   end
   get 'dashboard' => 'admin#dashboard'
+  get 'user/referral' => 'admin#referral'
+  get 'user/my_direct' => 'admin#direct_team'
+  get 'user/wallet' => 'admin#wallet_transfer'
+  get 'user/upgrade' => 'admin#upgrade'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
