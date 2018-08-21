2c8cf19811c1991f8788bcde84f4ed4433273778
Shashi Singh <shashi.we@gmail.com>
2018-08-21 18:28:27 +0530

    added level and transaction pages histroy for level

--- app/controllers/wallet_transactions_controller.rb
+++ app/controllers/wallet_transactions_controller.rb
@@ -24,10 +24,10 @@ class WalletTransactionsController < ApplicationController
           WalletTransaction.transaction_histroy(current_user, @transaction)
 
           # Add tranfered money
-          WalletTransaction.add_money(current_user, @transaction)
+          add_money = WalletTransaction.add_money(current_user, @transaction)
 
           # Dedcut tranfered money
-          WalletTransaction.deduct_money(current_user, @transaction)
+          dedcut_money = WalletTransaction.deduct_money(current_user, @transaction)
 
           format.html { redirect_to new_wallet_transaction_path, notice: "Successfully transfer to #{transfer_to.user_name}." }
         else
@@ -39,10 +39,34 @@ class WalletTransactionsController < ApplicationController
     end
   end
 
+  def upgrade_level
+    @transaction = WalletTransaction.new
+    @transactions = WalletTransaction.where(user_id: current_user.id)
+  end
+
+  def create_level_transactions
+    @transaction = WalletTransaction.new(wallet_transaction_params)
+    respond_to do |format|
+      if @transaction.save
+        # Dedcut tranfered money
+        WalletTransaction.deduct_money(current_user, @transaction)
+
+        level = @transaction.level.split(" ")[2]
+        amount = @transaction.amount.split(" ")[2]
+
+        @transaction.update_attributes(level: level, amount: amount, transfer_to: level, debit: amount, credit: 0, open_balance: current_user.wallet.amount, close_balance: 0, remark: "Level #{level} Remain", status: 1)
+
+        format.html { redirect_to levels_new_path, notice: "Successfully upgraded to." }
+      else
+        format.html { render 'upgrade_level' }
+      end
+    end
+  end
+
   private
 
   def wallet_transaction_params
-    params.require(:wallet_transaction).permit(:mobile_no, :amount, :transfer_by, :transfer_to, :user_id, :wallet_id)
+    params.require(:wallet_transaction).permit(:mobile_no, :amount, :transfer_by, :transfer_to, :user_id, :wallet_id, :level)
   end
 
 end
--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -17,6 +17,11 @@ class WalletTransaction < ApplicationRecord
     return user
   end
 
+  def self.transaction_histroy(current_user, transaction)
+    user = User.find_by(mobile_number: transaction.mobile_no)
+    WalletTransaction.create(mobile_no: current_user.mobile_number, amount: transaction.amount, transfer_by: current_user.user_name, transfer_to: user.user_name, user_id: user.id, wallet_id: user.wallet.id, debit: 0, credit: transaction.amount, open_balance: user.wallet.amount, close_balance: 0, remark: "Recevied #{transaction.amount} from #{current_user.user_name}", status: 1)
+  end
+
   def self.add_money(current_user, transaction)
     user = User.find_by(mobile_number: transaction.mobile_no)
     wallet = Wallet.find_by(user_id: user.id)
@@ -30,11 +35,6 @@ class WalletTransaction < ApplicationRecord
     wallet = Wallet.find_by(user_id: user.id)
     total_amount = wallet.amount.to_i - transaction.amount.to_i
     wallet.update_attributes(amount: total_amount)
-  end
-
-  def self.transaction_histroy(current_user, transaction)
-    user = User.find_by(mobile_number: transaction.mobile_no)
-    WalletTransaction.create(mobile_no: current_user.mobile_number, amount: transaction.amount, transfer_by: current_user.user_name, transfer_to: user.user_name, user_id: user.id, wallet_id: user.wallet.id, debit: 0, credit: transaction.amount, open_balance: user.wallet.amount, close_balance: 0, remark: "Recevied #{transaction.amount} from #{current_user.user_name}", status: 1)
   end
 
 end
