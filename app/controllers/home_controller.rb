1ea582b5c5ce23367eb4df6f51a8a8be60ce4c64
Shashi Singh <shashi.we@gmail.com>
2018-08-24 13:03:29 +0530

    quick fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
   layout 'admin', only: [:direct_team]
   before_action :authenticate_user!, only: [:direct_team]
-  
+
   def index
   end
 
   def direct_team
     @direct_team = current_user
-    @sponser = User.find_by(mobile_number: @direct_team.sponser_id) if @direct_team.sponser_id?
-    @transaction_amount = WalletTransaction.find_by(mobile_no: @sponser.mobile_number,transfer_by: current_user.user_name, transfer_to: @sponser.user_name)
-    @level = WalletTransaction.where('level = ? ', "1")
+    if @direct_team.sponser_id.present?
+      @sponser = User.find_by(mobile_number: @direct_team.sponser_id) 
+      @transaction_amount = WalletTransaction.find_by(mobile_no: @sponser.mobile_number,transfer_by: current_user.user_name, transfer_to: @sponser.user_name)
+      @level = WalletTransaction.where('level = ? ', "1")
+    end
   end
 end
