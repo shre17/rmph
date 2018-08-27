f0851b799e16fec305ef1124b066c885fa6ddac5
Shashi Singh <shashi.we@gmail.com>
2018-08-27 19:43:31 +0530

    quick fix

--- app/controllers/wallet_transactions_controller.rb
+++ app/controllers/wallet_transactions_controller.rb
@@ -77,7 +77,7 @@ class WalletTransactionsController < ApplicationController
           # Dedcut tranfered money
           dedcut_money = WalletTransaction.deduct_money(current_user, @transaction)
 
-          @transaction.update_attributes(level: level, amount: amount, transfer_to: level, debit: amount, credit: 0, open_balance: current_user.wallet.amount, close_balance: dedcut_money, remark: "Level #{level} Remain #{amount*@child_users}", status: 1)
+          @transaction.update_attributes(level: level, amount: amount, transfer_to: level, debit: amount, credit: 0, open_balance: current_user.wallet.amount, close_balance: dedcut_money, remark: "Level #{level} Remain", status: 1)
 
           format.html { redirect_to levels_new_path, notice: "Successfully upgraded to." }
         else
